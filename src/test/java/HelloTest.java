555b888e2a5de531fdc5dc27c95c363335cbd132
Nezihe Kisac <71877269+nezihekisac@users.noreply.github.com>
2021-06-14 12:48:39 +0100

    added more hello feature

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -3,6 +3,8 @@ public class HelloTest {
 
         System.out.println("Hello");
         System.out.println("Developing Hello feature");
+        System.out.println("Local Repo is already published to GitHub");
+        System.out.println("Doing more work");
 
 
 
